5721072eb18d5a2368e851a8f9ab45267c7d8c64
Ivelin Pavlov <ipavlov@objectsystems.com>
2020-11-05 14:34:14 +0200

    "adorable.io" server is no longer available. Use "hello-avatar.com/adorables" instead thanks to @gfauchart at https://github.com/adorableio/avatars-api-middleware/issues/108#issuecomment-709593363

--- src/LoggerAvatar.php
+++ src/LoggerAvatar.php
@@ -4,14 +4,14 @@ namespace OsiOpenSource\LaravelTeamsLogging;
 
 class LoggerAvatar
 {
-    const EMERGENCY = 'https://api.adorable.io/avatars/face/eyes7/nose7/mouth7/721C24';
-    const ALERT     = 'https://api.adorable.io/avatars/face/eyes7/nose7/mouth6/721C24';
-    const CRITICAL  = 'https://api.adorable.io/avatars/face/eyes7/nose7/mouth5/721C24';
-    const ERROR     = 'https://api.adorable.io/avatars/face/eyes7/nose7/mouth9/721C24';
-    const WARNING   = 'https://api.adorable.io/avatars/face/eyes6/nose7/mouth10/721C24';
-    const NOTICE    = 'https://api.adorable.io/avatars/face/eyes6/nose7/mouth3/721C24';
-    const INFO      = 'https://api.adorable.io/avatars/face/eyes5/nose7/mouth1/721C24';
-    const DEBUG     = 'https://api.adorable.io/avatars/face/eyes5/nose7/mouth1/721C24';
+    const EMERGENCY = 'https://api.hello-avatar.com/adorables/face/eyes7/nose7/mouth7/721C24';
+    const ALERT     = 'https://api.hello-avatar.com/adorables/face/eyes7/nose7/mouth6/721C24';
+    const CRITICAL  = 'https://api.hello-avatar.com/adorables/face/eyes7/nose7/mouth5/721C24';
+    const ERROR     = 'https://api.hello-avatar.com/adorables/face/eyes7/nose7/mouth9/721C24';
+    const WARNING   = 'https://api.hello-avatar.com/adorables/face/eyes6/nose7/mouth10/721C24';
+    const NOTICE    = 'https://api.hello-avatar.com/adorables/face/eyes6/nose7/mouth3/721C24';
+    const INFO      = 'https://api.hello-avatar.com/adorables/face/eyes5/nose7/mouth1/721C24';
+    const DEBUG     = 'https://api.hello-avatar.com/adorables/face/eyes5/nose7/mouth1/721C24';
 
     /** @var string */
     private $const;
